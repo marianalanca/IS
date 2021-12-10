f57f3c4fd55cdd047d65e79c793c976010599168
MarianaLoreto30 <marianaLoreto30@gmail.com>
2021-12-10 23:52:58 +0000

    admin cli :/ não reconhece o ClientBuilder :(

--- Ficha_3/adminCLI/src/main/java/restAPI/App.java
+++ Ficha_3/adminCLI/src/main/java/restAPI/App.java
@@ -1,6 +1,8 @@
 package restAPI;
 
+import java.awt.*;
 import java.util.List;
+import java.util.Scanner;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -11,16 +13,65 @@ import javax.ws.rs.core.Response;
 //import beans.Person;
 
 public class App {
+
     public static void main(String[] args) {
 
+        Scanner myObj = new Scanner(System.in);
+        int option = -1;
+        String aux;
 
         Client client = ClientBuilder.newClient();
 
-        WebTarget target = client.target("http://localhost:8080/restAPI/rest/myservice/addManager"); //tenho duvida no caminho
-        Response response = target.request().get();
-        String value = response.readEntity(String.class);
-        System.out.println("RESPONSE1: " + value);
-        response.close();
+        while(option != 0){
+
+            System.out.println("WELCOME\n");
+            System.out.println("MENU\n 0.Exit\n 1. Add Manager\n 2. Add Client\n 3. Add Currency\n" +
+                    " 4. List Managers\n 5. List Clients\n 6. List Currencies\n" +
+                    " 7. Get credit per client\n 8. Get payments\n" +
+                    " 9. Get the current balance of a client\n10. Get total\n" +
+                    "11. Get total payments\n 12. Get total balance\n" +
+                    "13. Compute the bill for each client for the last month\n" +
+                    "14. Get list of clients without payments for the last two months\n" +
+                    "15. Get person with the highest outstanding debt\n" +
+                    "16. Get manager who has made the highest revenue in payments from his clients\n\n");
+
+            do{
+                aux = myObj.nextLine();
+                try{
+                    option = Integer.parseInt(aux);
+
+                    switch (option){
+                        case(0):
+                            return;
+                        case(1):
+                            WebTarget target = client.target("http://localhost:8080/restAPI/rest/myservice/addManager"); //tenho duvida no caminho
+                            Response response = target.request().get();
+                            String value = response.readEntity(String.class);
+                            System.out.println("RESPONSE1: " + value);
+                            response.close();
+                            //cenas
+                            break;
+                        case(2):
+                            //cenas
+                            break;
+                        case(3):
+                            //cenas
+                            break;
+                        default:
+                            System.out.println("Invalid option. Try again");
+                    }
+
+                }
+                catch(Exception e){
+                    System.out.print("Error in parse.\n Try again: ");
+                }
+
+            }while(option == -1);
+
+        }
+
+
+        /*
 
         target = client.target("http://localhost:8080/restAPI/rest/myservice/addClient");
         target = target.queryParam("name", "xpto");
@@ -56,6 +107,9 @@ public class App {
         System.out.println("RESPONSE6: " + personList);
         response.close();
         */
+
+        myObj.close();
     }
+
 }
 
